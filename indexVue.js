449419a1833790f9c69b51d6eb49bedeab8ad762
JorisMLN <monleon.joris@gmail.com>
2021-03-05 16:28:57 +0100

    elt.style / elt.innerHTML

--- indexVue.js
+++ indexVue.js
@@ -1,18 +1,22 @@
-
+/* Variables */
 const elt = document.getElementById("idtest");
+
+/* Script */
 elt.addEventListener('click', function() {
     elt.style.color = "#457898";
+    elt.innerHTML = ("ordre66");
 });
 
 
-const app = new Vue({
-    el: '#app',
-    data:{
-        intro: "It's a good place to die, but not today",
+/* Vue */
+// const app = new Vue({
+//     el: '#app',
+//     data:{
+//         intro: "It's a good place to die, but not today",
 
-    },
-    computed: {
+//     },
+//     computed: {
         
-    }
-});
+//     }
+// });
 
