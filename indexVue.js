68bcb3709d31a0933a126c8e737d3405822f985d
JorisMLN <monleon.joris@gmail.com>
2021-03-05 15:40:57 +0100

    1er listener'click' JS

--- indexVue.js
+++ indexVue.js
@@ -1,9 +1,18 @@
+
+const elt = document.getElementById("idtest");
+elt.addEventListener('click', function() {
+    elt.style.color = "#457898";
+});
+
+
 const app = new Vue({
     el: '#app',
     data:{
-        intro: "It's a good place to die, but not today"
+        intro: "It's a good place to die, but not today",
+
     },
     computed: {
         
     }
 });
+
